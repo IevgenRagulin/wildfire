fb56b212b956074f02de46b8850cf56b74d8a997
JoeSuzuki <joesuzuki516@gmail.com>
2018-07-01 08:57:12 -0400

    Finished Subscribe view: needs to be connected with DB

--- src/Subscribe.js
+++ src/Subscribe.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { FormGroup, FormControl, ControlLabel, Form, Col } from 'react-bootstrap';
+import MaskedFormControl from 'react-bootstrap-maskedinput'
 import './Witness.css';
 
 class Subscribe extends React.Component {
@@ -8,6 +9,7 @@ class Subscribe extends React.Component {
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this._onChange = this._onChange.bind(this);
 
     this.state = {
       value: ''
@@ -17,7 +19,7 @@ class Subscribe extends React.Component {
   handleSubmit(event) {
     event.preventDefault();
     const data = new FormData(event.target);
-    console.log("_____");
+    console.log("Needs to be connected with DB");
     // fetch('https://desolate-anchorage-86223.herokuapp.com/witness/', {
     //   method: 'POST',
     //   headers: {
@@ -35,29 +37,31 @@ class Subscribe extends React.Component {
     this.setState({ value: e.target.value });
   }
 
+  
+  _onChange = (e) => {
+    this.setState({value: e.target.value});
+  }
+
   render() {
     return (
       <Form onSubmit={this.handleSubmit} className="form-signin widths">
         <h1>Subscribe</h1>
-        <FormGroup controlId="formControlsTextarea">
-          <ControlLabel>Recieve alerts from around the area</ControlLabel>
-          <FormControl 
-            componentClass="textarea" 
-            placeholder="###-###-###"
-            inputRef={input => this.phoneinput = input}
+        <FormGroup>
+          <ControlLabel>Phone Number</ControlLabel>
+          <MaskedFormControl 
+            type='text' 
+            name='phoneNumber' 
+            mask='111-111-1111'
+            id="phoneNumber" 
+            value={this.state.phoneNumber}
+            onChange={this._onChange}
           />
-          <FormControl.Feedback />
         </FormGroup>
         <FormGroup>
           <Col >
             <button className="btn btn-lg btn-primary btn-block" type="submit">Submit Witness</button>
           </Col>
         </FormGroup>
-        <FormGroup>
-        <ControlLabel>Phone Number</ControlLabel>
-        <MaskedFormControl type='text' name='phoneNumber' mask='111-111-1111' />
-      </FormGroup>
-
       </Form>
     );
   }
