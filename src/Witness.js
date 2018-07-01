b34f2ca59f4614a08a5ce4a24a0e6711c7d4a0d2
JoeSuzuki <joesuzuki516@gmail.com>
2018-07-01 11:50:50 -0400

    Popup notification after sending Witness form

--- src/Witness.js
+++ src/Witness.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { FormGroup, FormControl, ControlLabel, Form, Col, Tooltip } from 'react-bootstrap';
+import { FormGroup, FormControl, ControlLabel, Form, Col, Modal, Button } from 'react-bootstrap';
 import './Witness.css';
 
 class Witness extends React.Component {
@@ -8,10 +8,16 @@ class Witness extends React.Component {
 
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleHide = this.handleHide.bind(this);
 
     this.state = {
-      value: ''
+      value: '',
+      show: false
     };
+  }
+
+  handleHide() {
+    this.setState({ show: false });
   }
 
   handleSubmit(event) {
@@ -68,8 +74,29 @@ class Witness extends React.Component {
 
         <FormGroup>
           <Col >
-            <button className="btn btn-lg btn-primary btn-block" type="submit">Send alert</button>
+            <button 
+              className="btn btn-lg btn-primary btn-block"           
+              onClick={() => this.setState({ show: true })}
+              type="submit">Send alert</button>
           </Col>
+          <Modal
+          show={this.state.show}
+          onHide={this.handleHide}
+          container={this}
+          aria-labelledby="contained-modal-title"
+        >
+          <Modal.Header closeButton>
+            <Modal.Title id="contained-modal-title">
+              Success!
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            We were able to alert people that are subscribed near the location.
+          </Modal.Body>
+          <Modal.Footer>
+            <Button onClick={this.handleHide}>Close</Button>
+          </Modal.Footer>
+        </Modal>
         </FormGroup>
       </Form>
     );
